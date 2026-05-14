5ef0b96208eb59e489c74b27ca0bbf9d4ede5d65
egusquiza31 <egusquiza.31@hotmail.com>
2026-05-13 22:11:00 -0500

    asignar delivery

--- app/models/delivery_guide.rb
+++ app/models/delivery_guide.rb
@@ -1,8 +1,7 @@
 class DeliveryGuide < ApplicationRecord
   include Sanitizable
-
   belongs_to :dispatch_order
-  belongs_to :driver
+  belongs_to :driver, class_name: 'LogisticsUser', foreign_key: 'driver_id'  # 👈 Cambia esto
   belongs_to :vehicle
   has_many :delivery_incidents, dependent: :destroy
 end
